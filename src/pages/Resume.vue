e487807e19014de3e291338c30afd54b4032006d
Edward Nguyen <eddy-nguyen@hotmail.com>
2021-11-09 19:24:54 -0500

    Updated resume

--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -7,17 +7,17 @@
                     <v-divider style="margin-bottom: 1rem;"></v-divider>
                     <div class="subheading"><b>Languages</b></div>
                     <ul class="subheading">
-                        <li>JavaScript, PHP, HTML5, CSS3, Java, C++</li>
+                        <li>JavaScript, TypeScript, PHP, HTML5, CSS3, Java, C++, Golang</li>
                     </ul>
                     <br>
                     <div class="subheading"><b>Frameworks/Libraries/Tools</b></div>
                     <ul class="subheading">
-                        <li>Vue.js, Vuex, Vuetify, AngularJS, Drupal 7, Jest, Cypress, Git, JIRA</li>
+                        <li>React, Vue, Vuex, AngularJS, GraphQL, Datadog, Jest, Cypress, Webpack, Swagger, Git, JIRA</li>
                     </ul>
                     <br>
                     <div class="subheading"><b>Databases</b></div>
                     <ul class="subheading">
-                        <li>MySQL, Microsoft SQL server</li>
+                        <li>PostgreSQL, MySQL, SQLite, Microsoft SQL server</li>
                     </ul>
                 </v-card>
             </v-flex>
@@ -26,41 +26,52 @@
                 <v-card style="padding: 3rem;" class="section-card">
                     <div class="headline">Work Experience</div>
                     <v-divider style="margin-bottom: 1rem;"></v-divider>
+
+                    <div class="subheading"><b>Enzuzo Inc. – Full Stack Developer | July 2020 – August 2021</b></div>
+                    <ul class="subheading">
+                        <li>Created a React app that allows Shopify users to obtain an automated Terms of Service and Privacy
+                            Policy page as one of the primary features to attract users to use the company’s services</li>
+                        <li>Developed data-driven questionnaire by parsing fields such as question type and title in a JSON file
+                            which produces a customized Terms of Service and Privacy Policy automatically based on the user’s
+                            answers, increasing user participation with personalized results</li>
+                        <li>Configured tools such as Webpack, Babel and ESLint to help unify 3 separate web apps, ensure quality
+                            code and compatibility for older browsers</li>
+                        <li>Implemented several database features by creating Swagger definitions and using Golang to easily
+                            query data for a user’s Privacy Policy and Terms of Service page while keeping the flexibility to easily
+                            add more custom fields</li>
+                        <li>Mentored co-ops by initiating pair-programming and having an open-door policy to speed up learning
+                            process</li>
+                        <li>Integrated Datadog for front-end monitoring to analyze recent uninstall data from users to identify any
+                            common causes, using this data to direct future projects to improve app retention</li>
+                    </ul>
+
+                    <br>
+
                     <div class="subheading"><b>SSIMWAVE Inc. – Front-End Developer | May 2019 – August 2019</b></div>
                     <ul class="subheading">
-                        <li>Collaborated with team to overhaul front-end from AngularJS to Vue.js</li>
-                        <li>Helped develop scheduling functionality in Vue.js and Express.js</li>
-                        <li>Tasked with trimming down bug related tickets in JIRA</li>
-                        <li>Wrote unit tests and end-to-end tests </li>
-                        <li>Worked efficiently in an agile environment</li>
+                        <li>Collaborated with engineering and design team to overhaul front-end from AngularJS to Vue</li>
+                        <li>Modernized the UI by transforming design mock-ups to actual code while also taking steps towards
+                            unifying different apps to use the same architectural designs such as tech stack and file structure</li>
+                        <li>Helped develop a new scheduling functionality by using design mock-ups to implement in Vue.js and
+                            Express.js and prioritized ease of use by referencing various calendars in order to attract more users
+                            with the app’s expansive set of features</li>
+                        <li>Expanded automated testing by adding an end-to-end testing framework like Cypress to test more
+                            involved workflows which allowed for larger test coverage on top of unit tests</li>
                     </ul>
+
                     <br>
+
                     <div class="subheading"><b>University of Waterloo – Full Stack Developer | September 2018 – December 2018</b></div>
                     <ul class="subheading">
-                        <li>Communicated with the co-op department to develop a paging widget</li>
-                        <li>Collaborated with team members to design and develop web pages in Vue.js using Vuex and Vuetify for Portal 2.0</li>
-                        <li>Implemented RESTful API’s using ASP.NET (C#)</li>
-                        <li>Wrote unit tests for Vue.js web pages </li>
-                        <li>Developed a working prototype of an instant messaging widget for Portal 1.0 as an 80/20 project</li>
+                        <li>Assisted in developing a paging widget to replace the old co-op interview calling system, improving
+                            efficiency by automating tasks and making intuitive design targeted for new users</li>
+                        <li>Aided in modernizing the front-end design and implementation by incorporating student suggestions
+                            and university branding to improve overall look and feel</li>
+                        <li>Developed a working prototype of an instant messaging widget for Portal 1.0 as an 80/20 project,
+                            learning how sockets and instant messaging work, with plans to integrate the improved model into
+                            Portal 2.0</li>
                     </ul>
                     <br>
-                    <div class="subheading"><b>University of Waterloo - Web Developer | May 2017 – May 2018</b></div>
-                    <ul class="subheading">
-                        <li>Responsible for creating a custom Drupal module to extend the current basic Web form functionality</li>
-                        <li>Tasked to continue the PHP-based part-time worker submission application</li>
-                        <li>Efficiently worked in both virtual and public development environments to ensure quality control</li>
-                        <li>Collaborated with other team members such as Senior Developers, Digital Designers, and Project Managers</li>
-                    </ul>
-                    <br>
-                </v-card>
-            </v-flex>
-
-            <v-flex xs12 style="margin: 2rem 0 2rem 0">
-                <v-card style=" padding: 3rem;" class="section-card">
-                    <div class="headline">Education</div>
-                    <v-divider style="margin-bottom: 1rem;"></v-divider>
-                    <div class="subheading">Bachelor of Computer Science, Computer Science, University of Waterloo,
-                        Waterloo, Ontario, September 2014 – December 2019</div>
                 </v-card>
             </v-flex>
 
@@ -75,6 +86,24 @@
                         <li>Tasked with building the front-end</li>
                         <li>Spent 36 hours to learn React from scratch</li>
                     </ul>
+
+                    <br>
+
+                    <div class="subheading"><b>Listmaker</b></div>
+                    <ul class="subheading">
+                        <li>Motivated to learn how GraphQL and Auth0 works</li>
+                        <li>Retrieves restaurants from Yelp and allows you to add restaurant to your list on your account</li>
+                        <li>Uses your geolocation to provide you with restaurants nearby to provide a more personal experience</li>
+                    </ul>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 style="margin: 2rem 0 2rem 0">
+                <v-card style=" padding: 3rem;" class="section-card">
+                    <div class="headline">Education</div>
+                    <v-divider style="margin-bottom: 1rem;"></v-divider>
+                    <div class="subheading">Bachelor of Computer Science, Computer Science, University of Waterloo,
+                        Waterloo, Ontario, September 2014 – December 2019</div>
                 </v-card>
             </v-flex>
         </v-layout>
